b372533b3465ffd37c744dbf0d980863ac35af6f
yoratoni <yoratoni.dev@gmail.com>
2024-04-12 02:22:50 +0200

    Update

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center flex-col">
             <h1 className="text-5xl font-bold">Nextron Boilerplate</h1>
-            <h4 className="text-xl font-semibold">by Yoratoni</h4>
+            <h4 className="text-xl font-semibold">by Yoratoni.</h4>
 
             <p className="pt-4 max-w-sm text-center font-medium text-sm text-gray-400">
                 Here&apos;s a quick demo of the IPC bridge & router system in action.
